60ce0e72156441b57bc77bdcdca093324d48dc14
Alex Keith <alex.michael.keith@gmail.com>
2022-03-10 15:32:09 -0500

    global

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./topbar.scss"
 
-export default function TopBar() {
+export default function topbar() {
   return (
     <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">Intro</a>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
       dfg
     </div>
   )
